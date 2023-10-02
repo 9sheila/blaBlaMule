68e426cb5c1c1dd447aa3b53e5a8d50c91a32b89
Sheila Hernández <shey.hdez1999@gmail.com>
2023-10-02 19:27:03 +0200

    travel controller casi hecho

---
+++ controllers/travel.controller.js
@@ -0,0 +1,21 @@
+const createHttpError = require('http-errors');
+const Travel = require('../models/travel.model');
+const { StatusCodes } = require('http-status-codes');
+
+module.exports.getFilteredTravels = (req, res, next) => {
+    // const {id} = req.params;
+    const today = new Date();  // Current date
+    const endDate = new Date('2023-12-31');  // Replace with your desired end date
+    
+    Travel.find({
+      date: {
+        $gte: today,
+        $lte: endDate
+      }
+    })
+    .then((travels) => {
+        res.json(travels)
+    })
+    .catch(next)
+}
+       
